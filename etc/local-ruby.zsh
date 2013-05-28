1972aeebe9a53f8955ace4e92c5be0259d8d6937
Jianing Yang <jianingy.yang@gmail.com>
2013-05-28 11:33:13 +0800

    use rbenv for ruby environment

--- etc/local-ruby.zsh
+++ etc/local-ruby.zsh
@@ -1,4 +1,9 @@
-GEM_HOME=(~/local/site_ruby)
-[[ ! -d $GEM_HOME ]] && return
-export GEM_HOME
-path=($GEM_HOME/bin $path)
+#GEM_HOME=(~/local/site_ruby)
+#[[ ! -d $GEM_HOME ]] && return
+#export GEM_HOME
+#path=($GEM_HOME/bin $path)
+
+if which &>/dev/null rbenv; then
+	eval "$(rbenv init - zsh)"
+fi
+
