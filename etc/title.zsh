2ae8cd11e31d69148d731885815d5436f43dabdb
Jianing YANG <jianingy@unitedstack.com>
2013-06-29 15:26:43 +0800

    update title

--- etc/title.zsh
+++ etc/title.zsh
@@ -2,7 +2,7 @@
 
 precmd () {
 	label="${PWD/${HOME}/~}"
-	[ -n "$SSH_CONNECTION" ] && label="$HOST:label"
+	[ -n "$SSH_CONNECTION" ] && label="${HOST}:${label}"
 	echo -ne "\e]2;${label}\a"
 	echo -ne "\e]1;${label:-24}\a"
 }
